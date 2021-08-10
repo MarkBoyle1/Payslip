81237f762bd92755933b6205bbdaaff7195e3854
MarkBoyle1 <mark.boyle@myob.com>
2021-08-10 16:35:34 +1000

    Added user input for all fields and stored them in variables

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace payslip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the payslip generator!");
+            Console.WriteLine("Please input your first name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please input your surname:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Please input your annual salary:");
+            string annualSalary = Console.ReadLine();
+            Console.WriteLine("Please input your super rate:");
+            string superRate = Console.ReadLine();
+            Console.WriteLine("Please input your payment start date:");
+            string paymentStartDate = Console.ReadLine();
+            Console.WriteLine("Please input your payment end date:");
+            string paymentEndDate = Console.ReadLine();
+            
         }
     }
 }
